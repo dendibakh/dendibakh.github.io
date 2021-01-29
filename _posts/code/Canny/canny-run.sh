972d1fb5d85e8c58aa3ff097e490f1f44d9d26ef
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2021-01-29 08:15:49 +0100

    Add canny source and build script

---
+++ _posts/code/Canny/canny-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o xtrace
+
+wget --no-clobber https://johnysswlab.com/downloads/221575.tar.gz
+tar -xf 221575.tar.gz
+
+CC=clang
+CFLAGS="-O3 -lm -g -ffast-math"
+
+${CC} ${CFLAGS}  canny_source.c -o canny
+
+time ./canny 221575.pgm 0.5 0.7 0.9
