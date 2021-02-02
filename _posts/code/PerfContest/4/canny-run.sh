c28f88bff08508bdbd142f0634f7f808794c5067
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2021-02-02 20:48:54 +0100

    Add validation

--- _posts/code/PerfContest/4/canny-run.sh
+++ _posts/code/PerfContest/4/canny-run.sh
@@ -7,7 +7,10 @@ tar -xf 221575.tar.gz
 
 CC=clang
 CFLAGS="-O3 -ffast-math -march=core-avx2 -lm"
+CXXFLAGS="${CFLAGS} -lstdc++"
 
 ${CC} ${CFLAGS}  canny_source.c -o canny
+${CC} ${CXXFLAGS} validate.cpp -o validate
 
 time ./canny 221575.pgm 0.5 0.7 0.9
+./validate 221575.pgm_s_0.50_l_0.70_h_0.90.pgm.orig 221575.pgm_s_0.50_l_0.70_h_0.90.pgm
